c485c9a08b1a753de939d114c4449b153eae0aa4
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-11 09:43:19 -0700

    Started the primary API request.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,9 @@
 var tmdbAPIKey = "1564618e239b625cf432bde81f3e2494"
 
 
-
-
-fetch('https://api.themoviedb.org/3/movie/550?api_key=1564618e239b625cf432bde81f3e2494')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-
-// This url pulls movie rated from the worst to the best. Currently includeds foreign films also.
+// retrieves list of movies from worst to best.
 fetch('https://api.themoviedb.org/3/discover/movie?api_key=1564618e239b625cf432bde81f3e2494&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=500&with_watch_monetization_types=flatrate')
   .then(response => response.json())
   .then(data => console.log(data));
-
 
 

8c9751fcb9aa1643f7f8090367c78d3d3561692b
B.O.A Addington <Franswarduvar>
2022-07-13 18:54:35 -0700

    Commiting init UI JS for event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,92 @@
-var tmdbAPIKey = "1564618e239b625cf432bde81f3e2494"
+//Variables for the different inputs
+const OratedREL = document.getElementById('OratedR');
+const EratedREL = document.getElementById('EratedR');
+const YearinputEL = document.getElementById('YearInput');
+const HorrorEl = document.getElementById('Horror');
+const ComedyEL = document.getElementById('Comedy');
+const ActionEL = document.getElementById('Action');
+const RomComEL = document.getElementById('RomCom');
+const DramaEL = document.getElementById('Drama');
+const SatireEL = document.getElementById('Satire');
+
+//User input interaction functions
+number = document.getElementById('YearInput');
+
+number.addEventListener('change', e =>{
+
+console.log("Change happened");
+
+})
+
+checkbox = document.getElementById('OratedR');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+        console.log("Unchecked");
+});
+
+checkbox = document.getElementById('EratedR');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+checkbox = document.getElementById('Horror');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+checkbox = document.getElementById('Comedy');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+checkbox = document.getElementById('Action');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+checkbox = document.getElementById('RomCom');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+checkbox = document.getElementById('Drama');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+checkbox = document.getElementById('Satire');
+checkbox.addEventListener('change', e => {
+
+    if(e.target.checked){
+        console.log("checked");
+    } else 
+    console.log("Unchecked");
+});
+
+//Listoner for the button
+generate.addEventListener('click', () => {
+    
+    console.log("Button clicked");
+    
+});

b69ff8db3bccd8eee65e1b169041b7eef97d6b13
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-18 19:13:38 -0700

    meh

---
+++ assets/js/localstorage.js
@@ -0,0 +1,36 @@
+
+
+// Two stored methods to store saved movies from the list to the local storage.
+
+// storeSaved();
+
+// function storeSaved() {
+//     savedMovies = document.getElementById('savedList').textContent;
+//     localStorage.setItem('savedMovies', JSON.stringify(savedMovies));
+//     console.log(savedMovies);
+// }
+
+storeSaved2()
+
+function storeSaved2() {
+    savedMovies = document.getElementById('savedList').getElementsByTagName('li'),
+        localStorage.setItem('savedList', JSON.stringify());
+    console.log(savedMovies);
+};
+
+
+// Render the saved movies in local storage back onto the html list when page loads....
+
+renderList();
+
+function renderList() {
+    var localMovie = JSON.parse(localStorage.getItem('savedMovies'));
+    console.log(savedMovies)
+    movieListItem.appendChild(("savedMovies").textContent);
+}
+
+// renderList();
+
+// function renderList() {
+
+// }

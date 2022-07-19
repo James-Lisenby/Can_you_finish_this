14264c6a1d07dde3607f6db896dff57c3dceeff5
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-18 18:09:37 -0700

    two methods to store data from list to localstorage

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -1,11 +1,26 @@
 
 
-storeSaved()
+// Two stored methods to store saved movies from the list to the local storage.
 
-function storeSaved() {
-    var storeMovie = document.getElementsb(".storedMovie").value;
-    localStorage.setItem("storeMovie", JSON.stringify(storeMovie));
-    console.log(storeMovie);
+// storeSaved()
 
-    // document.getelementbyTAG(ul)
-}
+// function storeSaved() {
+//     savedMovies = document.getElementById('savedList').textContent;
+//     localStorage.setItem('savedMovies', JSON.stringify(savedMovies));
+//     console.log(savedMovies);
+// }
+
+// storeSaved2()
+
+// function storeSaved2() {
+//     savedMovies = document.getElementById('savedList').getElementsByTagName('li'),
+//         localStorage.setItem('savedList', JSON.stringify());
+//     console.log(savedMovies);
+
+// };
+
+
+// Render the saved movies in local storage back onto the html list when page loads....
+
+
+
